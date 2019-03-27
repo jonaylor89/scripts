defb75ebe7941681a36bf9b06fc799c43058f9a6
John Naylor <jonaylor89@gmail.com>
2019-03-27 19:52:35 -0400

    bug fix

--- py-init.sh
+++ py-init.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-printf "[$1] create project structure\n"
+printf "[$1] creating project structure\n"
 
 # Make directory for python project
 mkdir $1
@@ -23,7 +23,7 @@ mkdir src
 
 # Add placeholder file to source with basic skeleton
 touch src/main.py
-printf "#!/usr/bin/env python3\n\ndef main():\n\tpass\n\nif __name__ == '__main__':\n\tmain()\n" > src/main.py
+printf "#!/usr/bin/env python3\n\ndef main():\npass\n\nif __name__ == '__main__':\nmain()\n" > src/main.py
 
 # Make README and populate it
 touch README.md
@@ -45,7 +45,7 @@ if test $(which git); then
   git commit -m "init"
 fi
 
-printf "\n[$1] setup complete!\n"
+printf "\n[$1] \x1b[32msetup complete!\x1b[m\n"
 
 
 
