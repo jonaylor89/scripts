ffdb09158d21df54d8f2316cfdff290349dd59eb
John Naylor <jonaylor89@gmail.com>
2019-03-27 19:40:08 -0400

    debugging

--- py-init.sh
+++ py-init.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 
+printf "[$1] create project structure\n"
+
 # Make directory for python project
 mkdir $1
 
 # Go into the new directory
 cd $1
 
-if test ! $(which git); then
+# Initialize git repository
+if test $(which git); then
+  printf "[$1] Initializing git repository\n"
   git init
 
   touch .gitignore
+else 
+  printf "[$1] git not installed on the system, skipping...\n"
 fi
 
 # Create source directory
@@ -22,8 +28,12 @@ echo "# $1" > README.md
 # Create setup file for distrobution
 touch setup.py
 
-if test ! $(which pipenv); then
+# Create virtual environment
+if test $(which pipenv); then
+  printf "[$1] initializing virtual environment\n"
   pipenv --python 3.7
+else 
+  printf "[$1] pipenv not isntall on the system, skipping...\n"
 fi
 
 
